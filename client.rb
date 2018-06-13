994668a71684c1e6ce1d3dc537e9263301e76b7a
Sylwester Cichocki <sylwek1502@gmail.com>
2018-06-14 01:43:44 +0200

    Wersja websocket
    Z uruchamianiem kodu i odsyłaniem wyniku

--- client.rb
+++ client.rb
@@ -1,20 +1,50 @@
-require 'socket'        # Sockets are in standard library
-#require 'subprocess'
+require 'rubygems'
+require 'websocket-client-simple'
 
-hostname = 'localhost'
-port = 2000
+content = File.read('program.rb')
 
-s = TCPSocket.open(hostname, port)
+ws = WebSocket::Client::Simple.connect 'ws://localhost:8080'
 
-f = open('myfile.rb', 'w') 
-
-while line = s.gets     # Read lines from the socket
-   puts line.chop       # And print with platform line terminator
-   #File.write('./testFile.rb', line)
-   f.puts(line)
+ws.on :message do |msg|
+  puts msg.data
 end
-sys = system ('start ruby "myfile.rb"', out: $stdout, err: :out)
-#sys = %x{start ruby "myfile.rb}
-puts(sys)
-puts(out)
-s.close                 # Close the socket when done
+
+ws.on :open do
+# ws.send 'hello!!!'
+ws.send content
+end
+
+ws.on :close do |e|
+  p e
+  exit 1
+end
+
+ws.on :error do |e|
+  p e
+end
+
+loop do
+  ws.send STDIN.gets.strip
+end
+
+
+# require 'socket'        # Sockets are in standard library
+# #require 'subprocess'
+
+# hostname = 'localhost'
+# port = 2000
+
+# s = TCPSocket.open(hostname, port)
+
+# f = open('myfile.rb', 'w') 
+
+# while line = s.gets     # Read lines from the socket
+#    puts line.chop       # And print with platform line terminator
+#    #File.write('./testFile.rb', line)
+#    f.puts(line)
+# end
+# #sys = system ('start ruby "myfile.rb"', out: $stdout, err: :out)
+# #sys = %x{start ruby "myfile.rb}
+# puts(sys)
+# puts(out)
+# s.close                 # Close the socket when done
--- server.rb
+++ server.rb
@@ -1,17 +1,65 @@
-require 'socket'                 # Get sockets from stdlib
-require 'json'
+require 'eventmachine'
+require 'websocket-eventmachine-server'
+
+PORT = (ARGV.shift || 8080).to_i
+$f = open('myfile2.rb', 'w')
+EM::run do
+  @channel = EM::Channel.new
+
+  puts "start websocket server - port:#{PORT}"
+
+  WebSocket::EventMachine::Server.start(:host => "0.0.0.0", :port => PORT) do |ws|
+    ws.onopen do
+      sid = @channel.subscribe do |mes|
+        ws.send mes
+        $f = open('myfile2.rb', 'w')
+      end
+      puts "<#{sid}> connect"
+
+      @channel.push "hello new client <#{sid}>"
+
+      ws.onmessage do |msg|
+        puts "<#{sid}> #{msg}"
+        if $f 
+            $f.puts(msg)
+            $f.close
+            system("ruby myfile2.rb > log.txt")
+            r = File.read('log.txt')
+            @channel.push r
+        end
+        
+        # @channel.push "<#{sid}> #{msg}"
+      end
+      
+      ws.onclose do
+        puts "<#{sid}> disconnected"
+        @channel.unsubscribe sid
+        @channel.push "<#{sid}> disconnected"
+        
+      end
+    end
+  end
+
+end
+  
+# end
+
+# require 'socket'                 # Get sockets from stdlib
 
 
-server = TCPServer.open(2000)    # Socket to listen on port 2000
+# server = TCPServer.open(2000)    # Socket to listen on port 2000
 
 
-content = File.read('program.rb')
 
-loop {                           # Servers run forever
-   Thread.start(server.accept) do |client|
-   #client.puts(Time.now.ctime)   # Send the time to the client
-   #client.puts "Closing the connection. Bye!"
-   client.puts(content)
-   client.close                  # Disconnect from the client
-   end
-}
+
+
+# content = File.read('program.rb')
+
+# loop {                           # Servers run forever
+#    Thread.start(server.accept) do |client|
+#    #client.puts(Time.now.ctime)   # Send the time to the client
+#    #client.puts "Closing the connection. Bye!"
+#    client.puts(content)
+#    client.close                  # Disconnect from the client
+#    end
+# }
